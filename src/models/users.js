6f48041cfd2bb7a97bdfe568acc8d1afc7446717
QuantumSheep <quantumsheep@outlook.fr>
2020-02-19 15:47:26 +0100

    feat(users): Add find_by_token function

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,7 @@
 import db from '../db'
 
+import * as tokens from './tokens'
+
 export const model = db.model('User', {
   username: { type: String, },
   email: { type: String, },
@@ -12,3 +14,17 @@ export const username_regex = /^[A-Za-z0-9_-]{3,20}$/
 export const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export const password_regex = /^.{5,}$/
+
+/**
+ * @param {string} token 
+ */
+export async function find_by_token(token) {
+  const { user } = await tokens.model.findOne({
+    value: token,
+    expires: { $gt: Date.now(), }
+  })
+
+  console.log(user)
+
+  return await model.findById(user)
+}
