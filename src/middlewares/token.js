518898c5e08abf6337d79c35fb9ddd0d05c49b61
QuantumSheep <quantumsheep@outlook.fr>
2020-02-20 10:13:25 +0100

    style(token-middleware): Simplify middleware typing

--- src/middlewares/token.js
+++ src/middlewares/token.js
@@ -1,7 +1,5 @@
 /**
- * @param {import('express').Request} req 
- * @param {import('express').Response} res 
- * @param {import('express').NextFunction} next 
+ * @type {import('express').RequestHandler} 
  */
 export default (req, res, next) => {
   req.token = req.headers?.authorization || req.cookies?.token || req.body?.token || req.query?.token
