1c5261f6f993ffd7e97dc9e78742bcad28ad2174
QuantumSheep <quantumsheep@outlook.fr>
2020-02-20 11:40:21 +0100

    feat(optimization): Clusterize the app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import cluster from 'cluster'
+import os from 'os'
+
 import express from 'express'
 import body_parser from 'body-parser'
 import helmet from 'helmet'
@@ -10,24 +13,40 @@ import * as routes from './routes'
 
 import { ErrorsGenerator } from './utils/errors'
 
-const app = express()
-app.set('trust proxy', true)
+const cpus = os.cpus().length
 
-app.use(helmet())
+if (cluster.isMaster) {
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
 
-app.use(body_parser.urlencoded({ extended: false }))
-app.use(body_parser.json())
+  cluster.on('online', (worker) => {
+    console.log(`${worker.process.pid.toString().padStart(5, ' ')} is starting`)
+  })
 
-app.use(token_middleware)
-app.use(auth_middleware)
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} terminated.`)
+  })
+} else {
+  const app = express()
+  app.set('trust proxy', true)
 
-app.use(cors())
+  app.use(helmet())
 
-app.use(Object.values(routes))
+  app.use(body_parser.urlencoded({ extended: false }))
+  app.use(body_parser.json())
 
-app.use('*', (req, res) => {
-  res.status(404).send(ErrorsGenerator.gen(['Not Found']))
-})
+  app.use(token_middleware)
+  app.use(auth_middleware)
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Running on http://localhost:${port}`))
+  app.use(cors())
+
+  app.use(Object.values(routes))
+
+  app.use('*', (req, res) => {
+    res.status(404).send(ErrorsGenerator.gen(['Not Found']))
+  })
+
+  const port = process.env.PORT || 3000
+  app.listen(port, () => console.log(`${process.pid.toString().padStart(5, ' ')} started running on http://localhost:${port}`))
+}
