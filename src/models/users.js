a1c2be14e6abcfb572f064f35b41acf2cb1c442f
NenetteGit <antonin.texier@gmail.com>
2020-03-03 00:29:52 +0100

    feat(validator): Update username length from 2 to 5

--- src/models/users.js
+++ src/models/users.js
@@ -11,7 +11,7 @@ export const model = db.model('User', {
   created: { type: Date, default: Date.now, },
 })
 
-export const username_regex = XRegExp('^[\\p{L}0-9_]{2,20}$');
+export const username_regex = XRegExp('^[\\p{L}0-9_]{5,20}$');
 
 export const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
