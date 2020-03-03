850b87302955bb31679538adebebe96f3a61d8f9
QuantumSheep <quantumsheep@outlook.fr>
2020-03-03 16:00:56 +0100

    feat(docker): Dockerize the API

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost:27017/tatakae', {
+const url = process.env.MONGO_URL || 'mongodb://localhost:27017'
+const db_name = process.env.DB_NAME || 'tatakae'
+
+mongoose.connect(`${url}/${db_name}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
