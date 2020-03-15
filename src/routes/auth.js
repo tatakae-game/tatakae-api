24f86a5508587ecacb6c4173b662aed4d6bc0bd6
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-15 14:22:11 +0100

    feat(auth): Improve login and register data security

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -21,21 +21,27 @@ router.post('/auth/register', guard({ auth: constants.NOT_AUTH }), async (req, r
   const { username, email, password } = req.body || {}
   const errors = new ErrorsGenerator()
 
-  if (users.username_regex.test(username)) {
-    const exists = await users.model.exists({ username })
-    errors.assert(!exists, "This username is already used.")
-  } else {
-    errors.push("The username does not match the conditions.")
-  }
+  errors.assert(typeof username === 'string', "Invalid username.")
+  errors.assert(typeof email === 'string', "Invalid email.")
+  errors.assert(typeof password === 'string', "The password does not match the conditions.")
 
-  if (users.email_regex.test(email)) {
-    const exists = await users.model.exists({ email })
-    errors.assert(!exists, "This email is already used.")
-  } else {
-    errors.push("The email is not valid.")
-  }
+  if (!errors.has_errors) {
+    if (users.username_regex.test(username)) {
+      const exists = await users.model.exists({ username })
+      errors.assert(!exists, "This username is already used.")
+    } else {
+      errors.push("The username does not match the conditions.")
+    }
 
-  errors.assert(users.password_regex.test(password), "The password does not match the conditions.")
+    if (users.email_regex.test(email)) {
+      const exists = await users.model.exists({ email })
+      errors.assert(!exists, "This email is already used.")
+    } else {
+      errors.push("The email is not valid.")
+    }
+
+    errors.assert(users.password_regex.test(password), "The password does not match the conditions.")
+  }
 
   if (errors.has_errors) {
     return res.send({
@@ -58,33 +64,35 @@ router.post('/auth/register', guard({ auth: constants.NOT_AUTH }), async (req, r
 router.post('/auth/login', guard({ auth: constants.NOT_AUTH }), async (req, res, next) => {
   const { username, password, keep_connected } = req.body || {}
 
-  let username_is_email = false
+  if (typeof username === 'string' && typeof password === 'string') {
+    let username_is_email = false
 
-  if (users.email_regex.test(username)) {
-    username_is_email = true
-  }
+    if (users.email_regex.test(username)) {
+      username_is_email = true
+    }
 
-  if (users.password_regex.test(password)) {
-    try {
-      const user = await users.model.findOne({
-        [username_is_email ? "email" : "username"]: username,
-      })
-
-      const valid = await hash.verify(user.password, password)
-
-      if (valid) {
-        const token = await tokens.create(user._id, !!keep_connected)
-
-        return res.send({
-          success: true,
-          token: token.value,
-          user: {
-            id: user.id,
-            username: user.username,
-          },
+    if (users.password_regex.test(password)) {
+      try {
+        const user = await users.model.findOne({
+          [username_is_email ? "email" : "username"]: username,
         })
-      }
-    } catch { }
+
+        const valid = await hash.verify(user.password, password)
+
+        if (valid) {
+          const token = await tokens.create(user._id, !!keep_connected)
+
+          return res.send({
+            success: true,
+            token: token.value,
+            user: {
+              id: user.id,
+              username: user.username,
+            },
+          })
+        }
+      } catch { }
+    }
   }
 
   res.send(ErrorsGenerator.gen(['Credentials do not match.']))
