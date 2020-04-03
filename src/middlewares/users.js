b86b1866b625e5ea85f4b1c0b2382371700cc4a5
Nathanael Demacon <nathanael.dmc@outlook.fr>
2020-04-03 18:09:10 +0200

    feat(user-middleware): Catch exceptions

--- src/middlewares/users.js
+++ src/middlewares/users.js
@@ -4,8 +4,9 @@ import * as users from '../models/users'
  * @type {import('express').RequestHandler} 
  */
 export default async (req, res, next) => {
-
-  req.user = await users.find_by_token(req.token)
+  try {
+    req.user = await users.find_by_token(req.token)
+  } catch {}
 
   next()
 }
