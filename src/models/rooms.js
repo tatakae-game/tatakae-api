4a7c7737dc25ad4d001a58ec104246f7d83563a9
NenetteGit <antonin.texier@gmail.com>
2020-04-06 11:18:09 +0200

    feat(models): Remove null default value.

--- src/models/rooms.js
+++ src/models/rooms.js
@@ -15,7 +15,7 @@ export const model = db.model('Room', {
   users: { type: [db.Types.ObjectId], default: [], },
   created: { type: Date, default: Date.now, },
   is_ticket: { type: Boolean, default: false, },
-  assigned_to: { type: db.Types.ObjectId, default: null },
+  assigned_to: { type: db.Types.ObjectId, },
 })
 
 export function sanitize_message(message) {
