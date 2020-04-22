4eab6ce621acabeaf8f9e70be461622107c3c140
Simon <shalimi5@reseau-ges.fr>
2020-04-22 21:19:51 +0200

    fix(game): Fixed returned data to match futur usages

--- src/services/wandbox.service.js
+++ src/services/wandbox.service.js
@@ -3,17 +3,19 @@ import * as urls from '../constants/api-url'
 
 const wandbox_url = urls.WANDBOX_URL
 
-export async function execute_code(code, args) {
+export async function execute_code(code) {
   try {
     const res = await axios.post(wandbox_url, {
-        code,
-        compiler: "nodejs-head",
-        ...args,
-      }
+      code,
+      compiler: "nodejs-head",
+    }
     )
 
-    console.log(res)
+    if (res.data.program_error) {
+      return res.data.program_error
+    }
 
+    return JSON.parse(res.data.program_output)
   } catch (e) {
     console.log(e)
   }
