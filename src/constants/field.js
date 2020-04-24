526cb25c636bd965c63fc4f8af6ca39738d8406d
Simon <shalimi5@reseau-ges.fr>
2020-04-24 13:22:25 +0200

    feat(game): Add maximum turn constant

--- src/constants/field.js
+++ src/constants/field.js
@@ -2,3 +2,4 @@ export const SELECTABLE_TILES = ['sand', 'grass', 'rock']
 export const SELECTABLE_OBSTACLE = ['ruins', 'desert-mountain', 'tree']
 export const SELECTABLE_SIZE = [6, 7, 8]
 export const MAX_OBSTACLE = 12
+export const END_TURN = 1
