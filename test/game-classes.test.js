ea5675d3ebc83088ce4fc0862ce7235bf151d911
Simon <shalimi5@reseau-ges.fr>
2020-04-28 17:34:17 +0200

    refactor(game): Clean game-classes test file

--- test/game-classes.test.js
+++ test/game-classes.test.js
@@ -1,8 +1,6 @@
-
 import { strict as assert, AssertionError } from 'assert'
 
-import gameClasses from '../src/socket-endpoints/game-classes'
-import { isSymbol } from 'util'
+import game_classes from '../src/socket-endpoints/game-classes'
 
 describe('robot', () => {
 
@@ -11,23 +9,23 @@ describe('robot', () => {
     describe('model', () => {
       it('should initialize robot depending of passed model', () => {
         const map = { square_size: 5 }
-        const robot = new gameClasses.Robot('default', map)
+        const robot = new game_classes.Robot('default', map)
 
-        assert.equal(robot.hp, gameClasses.Robot.models[robot.model].hp)
+        assert.equal(robot.hp, game_classes.Robot.models[robot.model].hp)
         assert.equal(robot.model, 'default')
       })
 
       it('should initialize robot as default if model doesnt exist', () => {
         const map = { square_size: 5 }
-        const robot = new gameClasses.Robot('cat', map)
+        const robot = new game_classes.Robot('cat', map)
 
         assert.equal(robot.model, 'default')
-        assert.equal(robot.hp, gameClasses.Robot.models[robot.model].hp)
+        assert.equal(robot.hp, game_classes.Robot.models[robot.model].hp)
       })
 
       it('should initialize robot memory map filled with undiscovered tiles', () => {
         const map = { square_size: 5 }
-        const robot = new gameClasses.Robot('default', map)
+        const robot = new game_classes.Robot('default', map)
 
         for (const undiscovered_map of robot.memory_map) {
           assert.equal(undiscovered_map, 'not_discovered')
