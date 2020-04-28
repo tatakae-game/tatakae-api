9c960b590c3ee6f69d5368d2d3053720fa8ce4d4
Simon <shalimi5@reseau-ges.fr>
2020-04-28 16:50:20 +0200

    feat(test): Written all Robot tests

--- src/socket-endpoints/game.js
+++ src/socket-endpoints/game.js
@@ -1,6 +1,12 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
 import token_middlware from '../socket-middlewares/token'
 import * as users from '../models/users'
 import * as wandbox from '../services/wandbox.service'
+import * as game_service from '../services/game.service'
+import gameClasses from './game-classes'
+import * as game_constants from '../constants/field'
 
 /**
  * @param {import('socket.io').Server} io 
@@ -11,25 +17,50 @@ export default (io) => {
   nsp.on('connection', async (socket) => {
     try {
 
+      /**
+       *  data for front instanciation
+       */
+      const field = game_service.generate_field()
+      const map = new gameClasses.Map(field)
+
       const user = await users.find_by_token(socket.token)
-      const opponent = await users.find_opponent(user)
+      const robot = new gameClasses.Robot(user.robot, field)
 
+      const opponent = socket.training ? null : await users.find_opponent(user)
+      opponent.code = `robot.walk(3)`
+      const opponent_robot = new gameClasses.Robot(opponent ? opponent.robot : 'default', field)
 
+      game_service.randomize_initial_robot_position(robot, opponent_robot, map)
 
-      socket.emit('match found', {
-        opponent_robot: opponent.robot,
-        robot: user.robot,
+      socket.emit('match found', await game_service.sanitize_game_info(user, robot, opponent, opponent_robot, field))
+
+      fs.readFile(path.resolve(__dirname, "./game-classes.js"), async (err, game_classes) => {
+        if (err) {
+          console.log(err)
+        } else {
+          const trimmed_classes = game_classes.toString().replace(/export.*/, '')
+          let actual_turn = 0
+
+          while (robot.hp > 0 && opponent_robot.hp > 0 && actual_turn < game_constants.END_TURN) {
+            game_service.resetRobots(robot, opponent_robot)
+
+            const user_round = await wandbox.execute_code(trimmed_classes + game_service.encapsulate_user_code(user.code, robot, opponent_robot))
+            game_service.update_robot(user_round, robot, opponent_robot)
+
+            const opponent_round = await wandbox.execute_code(trimmed_classes + game_service.encapsulate_user_code(opponent.code, opponent_robot))
+            game_service.update_robot(opponent_round, opponent_robot, robot)
+
+            actual_turn++
+            socket.emit('round_evenements', await game_service.sanitize_round_info(user_round, opponent_round))
+            console.log(user_round[0])
+          }
+
+          game_service.end_game(socket, robot, opponent_robot, user, opponent)
+        }
       })
 
-
-      console.log(opponent)
-
-      wandbox.execute_code('console.log("chat")', {chat: 'chat'})
     } catch (e) {
       console.log(e)
     }
   })
 }
-
-
-
--- test/game.service.test.js
+++ test/game.service.test.js
@@ -1,7 +1,1 @@
 import { strict as assert } from 'assert'
-
-describe('chat', () => {
-  describe('cat', () => {
-    assert(1, 1)
-  })
-})
