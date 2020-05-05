ee62ea494705373b11b00a081eb71400a97abf54
NenetteGit <antonin.texier@gmail.com>
2020-05-05 21:32:26 +0200

    feat(models): Create funtions to get default permissions and check existing permissions with default ones.

--- src/models/groups.js
+++ src/models/groups.js
@@ -1,11 +1,35 @@
 import db from '../db'
+import * as permissions from '../constants/permissions'
 
 export const Permission = new db.Schema({
   name: { type: String, },
-  value: { type: Boolean, },
+  value: { type: Boolean, default: false, },
 })
 
 export const model = db.model('Group', {
   name: { type: String, },
   permissions: { type: [Permission], default: [], },
 })
+
+export function get_default_permissions() {
+  return Object.values(permissions).map(str => {
+    return {
+      name: str,
+      value: false
+    }
+  })
+}
+
+export function permission_matcher(groups, default_permissions) {
+  for (const group of groups) {
+    const updated_permissions = default_permissions.map(permission => {
+      const found = group.permissions.find(p => p.name === permission.name)
+      if (found) {
+        permission.value = found.value
+      }
+      return permission
+    })
+    group.permissions = updated_permissions
+  }
+  return groups
+}
