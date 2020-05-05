c274c0dcb77361afc122dc7c6e54f4877d3f4843
NenetteGit <antonin.texier@gmail.com>
2020-05-05 21:29:22 +0200

    style(constants): Rename constant Admin to Dashboard.

--- src/constants/permissions.js
+++ src/constants/permissions.js
@@ -1,4 +1,4 @@
 export const PERMISSION_CHAT = 'Chat'
-export const PERMISSION_ADMIN = 'Admin'
+export const PERMISSION_DASHBOARD = 'Dashboard'
 export const PERMISSION_EDITOR = 'Editor'
 export const PERMISSION_GAME = 'Game'
