2125867f6d0144d6e16d614d672b0f3e9cca236a
NenetteGit <antonin.texier@gmail.com>
2020-06-01 20:36:11 +0200

    fix(sockets): Fix broadcast in all rooms.

--- src/socket-endpoints/chat.js
+++ src/socket-endpoints/chat.js
@@ -9,16 +9,30 @@ import * as rooms from '../models/rooms'
 export default (io) => {
   const nsp = io.of('/chat').use(token_middlware)
 
+  const namespaces = {}
+
   nsp.on('connection', async (socket) => {
     const room_id = socket.handshake.query?.room
 
     if (!room_id) {
       socket.disconnect()
     }
-    
+
     try {
       const room = await rooms.model.findById({ _id: room_id })
       const user = await users.find_by_token(socket.token)
+
+      if (room._id in namespaces) {
+        namespaces[room._id].push(socket.id)
+      } else {
+        namespaces[room._id] = [socket.id]
+      }
+
+      socket.on('disconnect', () => {
+        for (const id in namespaces) {
+          namespaces[id] = namespaces[id].filter(s => s !== socket.id)
+        }
+      })
 
       socket.on('message', message => {
         message.author = user.id;
@@ -26,7 +40,9 @@ export default (io) => {
         room.messages.push(message)
         room.save()
 
-        nsp.emit('new message', rooms.sanitize_message(message))
+        for (const id of namespaces[room._id]) {
+          nsp.sockets[id].emit('new message', rooms.sanitize_message(message))
+        }
       })
     } catch {
       console.error(`The room ${room_id} does not exist.`)
