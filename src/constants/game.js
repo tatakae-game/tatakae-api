387a3de0e3c448dee76febd77a6227427c2900a9
Simon Halimi <simonhalimi@Simons-MBP.home>
2020-06-24 18:57:08 +0200

    refacto(): Fix obstacle to rock

--- src/constants/game.js
+++ src/constants/game.js
@@ -1,5 +1,5 @@
 export const SELECTABLE_TILES = ['grass']
-export const SELECTABLE_OBSTACLE = ['ruins', 'desert-mountain', 'tree']
+export const SELECTABLE_OBSTACLE = ['rock']
 export const SELECTABLE_SIZE = [6, 7, 8]
 export const MAX_OBSTACLE = 12
 export const END_TURN = 10
