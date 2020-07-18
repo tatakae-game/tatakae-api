71f96b61e34359e3f26852b688159c6af3e3c71e
Simon Halimi <halimisimon@gmail.com>
2020-07-19 00:11:58 +0200

    feat(): San error handling implemeted

--- src/services/playrground_service.js
+++ src/services/playrground_service.js
@@ -3,37 +3,33 @@ import * as urls from '../constants/api-url'
 
 const playground_url = urls.PLAYGROUND_URL
 
-export async function execute_code(files, entrypoint, stdin) {
-    console.log("in exec")
-    console.log(entrypoint)
-    console.log(stdin)
-  try {
-    const res = await axios.post(`${playground_url}/run`, {
-      files,
-      stdin,
-      entrypoint
-    })
+export async function test(files, entrypoint, stdin) {
+    try {
+        const res = await axios.post(`${playground_url}/run`, {
+            "files": files,
+            "stdin": stdin,
+            "entrypoint": entrypoint,
+        })
 
-    // console.log(res)
+        return res.data.stderr
 
-    return res
-
-  } catch (e) {
-    // console.log(e)
-  }
+    } catch (e) {
+        console.log(e)
+    }
 
 }
 
-export async function build(files, stdin, entrypoint) {
-  try {
-    const res = await axios.post(`${playground_url}/build`, {
-        files,
-        stdin,
-        entrypoint,
-    })
+export async function execute_code(files, stdin, entrypoint) {
+    try {
+        const res = await axios.post(`${playground_url}/run`, {
+            "files": files,
+            "stdin": stdin,
+            "entrypoint": entrypoint,
+        })
 
-    return res
-} catch (e) {
-    console.log(e)
-  } 
+        return res.data
+
+    } catch (e) {
+        console.log(e)
+    }
 }
