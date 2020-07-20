d3d65630d4c3c0e038fb3786b5dd5b53b1c3afcc
quantumsheep <quantumsheep@users.noreply.github.com>
2020-07-20 12:11:48 +0200

    feat: Use SAN_PLAYGROUND_URL env variable

--- src/constants/api-url.js
+++ src/constants/api-url.js
@@ -1,2 +1,2 @@
 export const WANDBOX_URL = 'https://wandbox.org/api/compile.json'
-export const PLAYGROUND_URL = 'http://localhost:4000'
+export const PLAYGROUND_URL = process.env.SAN_PLAYGROUND_URL || 'http://localhost:4000'
